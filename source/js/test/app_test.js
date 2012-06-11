08c11851af511bd1191c81eb0fe2775221e5babb
Darren_N <info@v25media.com>
2012-06-11 15:21:06 -0400

    whitespace

--- source/js/test/app_test.js
+++ source/js/test/app_test.js
@@ -1,22 +1,22 @@
 define([
-    'jquery', 
-    'underscore',
-    'backbone',
-    'amplify_core',
-    'amplify_store'
+  'jquery', 
+  'underscore',
+  'backbone',
+  'amplify_core',
+  'amplify_store'
 ], function($, _, Backbone, amplify){
     return {
-        init: function(){
-            console.log($);
-            console.log(_);
-            console.log(Backbone);
-            console.log(amplify);
+      init: function(){
+        console.log($);
+        console.log(_);
+        console.log(Backbone);
+        console.log(amplify);
 
-            var test = _.map(['a','b','c'], function(letter){
-              return letter + "-arity";
-            })
+        var test = _.map(['a','b','c'], function(letter){
+          return letter + "-arity";
+        })
 
-            $('#target').html(test.join(', '));
-        }
+        $('#target').html(test.join(', '));
+      }
     };
 });
