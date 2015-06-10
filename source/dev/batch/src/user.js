a5a8ceda7cf374a4fd77c6c41fe035d320ade379
Liam Bowers <liamondrop@gmail.com>
2015-06-10 15:48:57 -0400

    Parse and validate user identity

--- source/dev/batch/src/user.js
+++ source/dev/batch/src/user.js
@@ -1,3 +1,18 @@
-var user = {};
+import _ from 'underscore';
+
+const userJSON = window.sessionStorage.getItem('user');
+const user = JSON.parse(userJSON);
+
+function validate() {
+  if (!_.has(user, 'digest') ||
+      !_.has(user, 'email') ||
+      !_.has(user, 'name') ||
+      !_.has(user, 'username')) {
+    document.location = '/#login';
+  }
+  return user;
+}
+
+user.validate = validate;
 
 export default user;
