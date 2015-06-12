14362d456b254a5dda7d55a02035919c2cf15d44
Liam Bowers <liamondrop@gmail.com>
2015-06-11 20:34:07 -0400

    Initializing the app

--- source/dev/batch/src/app.js
+++ source/dev/batch/src/app.js
@@ -1,12 +1,18 @@
 import app from 'ampersand-app';
 import user from './user';
+import Router from './router';
+
+const userNameNode = document.getElementById('user-name');
 
 app.extend({
   init() {
-    // this.me = new Me()
-    // this.me.fetchAll()
-    // this.router = new Router()
-    // this.router.history.start()
+    this.user = user.validate();
+    this.router = new Router({});
+    this.router.history.start({
+      pushState: false,
+      root: '/dev/batch/'
+    });
+    userNameNode.textContent = user.name;
     return this;
   }
 });
