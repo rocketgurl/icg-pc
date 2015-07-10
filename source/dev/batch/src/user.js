61e5f0358029df3ec81a9d5f5ed6223445f08d21
Liam Bowers <liamondrop@gmail.com>
2015-07-10 16:05:53 -0400

    Use presence of cookie token as key part of user validation for now

--- source/dev/batch/src/user.js
+++ source/dev/batch/src/user.js
@@ -1,10 +1,15 @@
 import _ from 'underscore';
+import Cookie from './lib/cookie';
+
+const cookie = new Cookie();
+const token  = cookie.get('ics360_PolicyCentral') || '';
 
 const userJSON = window.sessionStorage.getItem('user');
 const user = JSON.parse(userJSON) || {};
 
 function validate() {
-  if (!_.has(user, 'digest') ||
+  if (!token.length ||
+      !_.has(user, 'digest') ||
       !_.has(user, 'email') ||
       !_.has(user, 'name') ||
       !_.has(user, 'username')) {
