79bbda3acfad6816f8fd8941f95df2cd3767a97b
Liam Bowers <liamondrop@gmail.com>
2015-07-29 13:42:43 -0400

    Updating various components

--- source/dev/batch/src/components/batches-table.js
+++ source/dev/batch/src/components/batches-table.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import app from 'ampersand-app';
+import BatchRow from './batch-row';
 
 export default React.createClass({
-
   render() {
     return (
-      <div className="div-table table-striped table-hover table-condensed table-scrollable table-5-columns">
+      <div className="div-table table-striped table-hover table-scrollable table-5-columns">
         <div className="thead">
           <div className="tr">
             <div className="th"><a href="status">Status <span className="glyphicon"></span></a></div>
@@ -17,15 +17,7 @@ export default React.createClass({
         </div>
         <div className="tbody" style={{maxHeight: `${500}px`}}>
           {this.props.batches.map((batch, index) => {
-            return (
-              <div key={index} className="tr">
-                <div className="td">Status</div>
-                <div className="td">{`${batch.processDefinitionKey} ${batch.id}`}</div>
-                <div className="td">Quantity</div>
-                <div className="td">{batch.startTime}</div>
-                <div className="td">{batch.startUserId}</div>
-              </div>
-              );
+            return <BatchRow key={batch.id} batch={batch}/>;
           })}
         </div>
       </div>
--- source/dev/batch/src/components/main.js
+++ source/dev/batch/src/components/main.js
@@ -3,38 +3,56 @@ import app from 'ampersand-app';
 import TabContent from './tab-content';
 import StackedBarChart from './stacked-bar-chart';
 import FormControls from './form-controls';
+import BatchActionSelect from './batch-action-select';
 import BatchesTable from './batches-table';
+import PoliciesTable from './policies-table';
 import {Nav, NavItem, TabPane} from 'react-bootstrap';
 
 export default React.createClass({
   getInitialState() {
-    return {batches: []};
+    return {
+      batches: [],
+      query: {
+        start: 0,
+        size: 50,
+        sort: 'startTime',
+        order: 'desc',
+        includeProcessVariables: true,
+        variables: [{
+          name : 'numPolicyRefs', // HACK: This should only return "batch" processes
+          operation : 'greaterThanOrEquals',
+          value : 0
+        }]
+      }
+    };
   },
 
   componentDidMount() {
     // app.batches.on('all', (...args) => { console.log('COLL', args) })
     app.batches.on('sync', this._onBatchesSync);
+    app.batches.query(this.state.query);
   },
 
   render() {
     const {tab} = this.props;
     return (
-      <div className="panel panel-default panel-nav">
-        <Nav bsStyle="tabs" activeKey={tab}>
-          <NavItem eventKey="batches" href="#batches">Batches</NavItem>
-          <NavItem eventKey="policies" href="#policies">Policies</NavItem>
-        </Nav>
-        <div className="panel-body">
-          <TabContent activeKey={tab}>
-            <TabPane key="batches">
-              <FormControls/>
-              <BatchesTable batches={this.state.batches}/>
-            </TabPane>
-            <TabPane key="policies">
-              <FormControls/>
-              <StackedBarChart/>
-            </TabPane>
-          </TabContent>
+      <div>
+        <BatchActionSelect onActionSelect={this._onActionSelect}/>
+        <div className="panel panel-default panel-nav">
+          <Nav bsStyle="tabs" activeKey={tab}>
+            <NavItem eventKey="batches" href="#batches">Batches</NavItem>
+            <NavItem eventKey="policies" href="#policies">Policies</NavItem>
+          </Nav>
+          <div className="panel-body">
+            <TabContent activeKey={tab}>
+              <TabPane key="batches">
+                <BatchesTable batches={this.state.batches}/>
+              </TabPane>
+              <TabPane key="policies">
+                <PoliciesTable/>
+              </TabPane>
+            </TabContent>
+          </div>
         </div>
       </div>
     );
@@ -42,5 +60,7 @@ export default React.createClass({
 
   _onBatchesSync(collection) {
     this.setState({batches: collection});
-  }
+  },
+
+  _onActionSelect() {}
 });
