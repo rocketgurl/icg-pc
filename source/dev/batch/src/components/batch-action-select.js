48b5a17fb8405d131d6c27409aa7c97fffcda3bb
Liam Bowers <liamondrop@gmail.com>
2015-07-29 13:45:17 -0400

    Batch action components

---
+++ source/dev/batch/src/components/batch-action-select.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default React.createClass({
+  render() {
+    return (
+      <div className="row">
+        <div className="col-xs-3">
+          <select className="form-control"
+            onChange={this.props.onActionSelect}>
+            <option>Select a Batch Action</option>
+            <option value="invoice">Invoice Policies</option>
+          </select>
+        </div>
+      </div>
+      );
+  }
+});
