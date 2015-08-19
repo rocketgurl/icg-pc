79533875a20d9f40d7efabdba5d3c7699649d85f
Liam Bowers <liamondrop@gmail.com>
2015-08-19 17:35:34 -0400

    a host of changes to policies table

--- source/dev/batch/src/components/policies-table.js
+++ source/dev/batch/src/components/policies-table.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import moment from 'moment';
 import sortableTableMixin from '../lib/sortable-table-mixin';
 import PolicyRow from './policy-row';
+import TableControls from './table-controls';
 
 export default React.createClass({
   mixins: [sortableTableMixin], // mixin common methods for sortable tables
 
   getInitialState() {
     return {
+      shouldAllItemsBeChecked: false,
       collection: [],
       query: {
         start: 0,
@@ -43,35 +46,67 @@ export default React.createClass({
 
   render() {
     const {startTime} = this.state.sortTable;
+    const {startedAfter, finishedBefore} = this.state.query;
+    console.log(this.state.query)
     return (
-      <div className="div-table table-striped table-hover table-scrollable table-sortable table-7-columns">
-        <div className="thead">
-          <div className="tr">
-            <div className="th"><input type="checkbox"/></div>
-            <div className="th">
-              <a data-sortby="startTime"
-                className={startTime.active ? startTime.order : null}
-                onClick={this._onHeaderClick}>
-                Time Started <span className="caret"/>
-              </a>
+      <div>
+        <TableControls onControlChange={this._onControlChange}/>
+        <div className="div-table table-striped table-hover table-scrollable table-sortable table-7-columns">
+          <div className="thead">
+            <div className="tr">
+              <div className="th">
+                <label className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    checked={this.state.shouldAllItemsBeChecked}
+                    onChange={this._onSelectAllToggle}/> Select All
+                </label>
+              </div>
+              <div className="th">
+                <a data-sortby="startTime"
+                  className={startTime.active ? startTime.order : null}
+                  onClick={this._onHeaderClick}>
+                  Time Started <span className="caret"/>
+                </a>
+              </div>
+              <div className="th">Policy #</div>
+              <div className="th">Batch ID</div>
+              <div className="th">Initiator</div>
+              <div className="th">Status</div>
+              <div className="th">Message</div>
             </div>
-            <div className="th">Policy #</div>
-            <div className="th">Batch ID</div>
-            <div className="th">Initiator</div>
-            <div className="th">Status</div>
-            <div className="th">Message</div>
           </div>
-        </div>
-        <div className="tbody" style={{maxHeight: `${500}px`}}>
-          {this.state.collection.map(policy => {
-            return <PolicyRow key={policy.id} policy={policy}/>;
-          })}
+          <div className="tbody" style={{maxHeight: `${500}px`}}>
+            {this.state.collection.map(policy => {
+              return (
+                <PolicyRow
+                  key={policy.id}
+                  policy={policy}
+                  itemShouldBeChecked={this.state.shouldAllItemsBeChecked}/>
+                );
+            })}
+          </div>
         </div>
       </div>
     );
   },
 
+  _onSelectAllToggle(e) {
+    this.setState({shouldAllItemsBeChecked: e.target.checked});
+  },
+
   _onCollectionSync(collection) {
     this.setState({collection});
+  },
+
+  _onControlChange(key, value) {
+    let {query} = this.state;
+    if (value === 'default') {
+      delete query[key];
+    } else {
+      query[key] = value;
+    }
+    this.setState({query});
+    this.makeQuery();
   }
 });
