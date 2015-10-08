a5d700947e1de94ecb179686c27ec82c1d843ef4
Liam Bowers <liamondrop@gmail.com>
2015-10-08 12:43:57 -0400

    better width for batch action select

--- source/js/modules/BatchProcessing/src/components/batch-action-select.js
+++ source/js/modules/BatchProcessing/src/components/batch-action-select.js
@@ -9,7 +9,7 @@ const BATCH_ACTIONS = [
 export default React.createClass({
   render() {
     return (
-      <div className="col-xs-2">
+      <div className="col-sm-3 col-xs-4">
         <select
           className="form-control"
           onChange={this._onActionSelect}
